88974b25ed026abdce5ba3b1c1e283cd0bd92b86
stone1985 <igevdo@mail.ru>
2026-04-06 23:55:25 +0300

    - announce interval = 30sec

--- src/ShortP2P.Discovery/UdpPeerDiscoveryOptions.cs
+++ src/ShortP2P.Discovery/UdpPeerDiscoveryOptions.cs
@@ -8,7 +8,7 @@ public sealed class UdpPeerDiscoveryOptions
     /// <summary>Порт, на котором слушаются и рассылаются beacon-пакеты.</summary>
     public int DiscoveryPort { get; set; } = 17890;
 
-    public TimeSpan AnnounceInterval { get; set; } = TimeSpan.FromSeconds(2);
+    public TimeSpan AnnounceInterval { get; set; } = TimeSpan.FromSeconds(30);
 
     public TimeSpan PeerStaleTimeout { get; set; } = TimeSpan.FromSeconds(30);
 
