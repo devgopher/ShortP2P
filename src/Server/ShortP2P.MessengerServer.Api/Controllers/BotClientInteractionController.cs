54644b25450b94991adaed0772dba496c0683a85
stone1985 <igevdo@mail.ru>
2026-09-25 14:57:32 +0400

    - v0.2

---
+++ src/Server/ShortP2P.MessengerServer.Api/Controllers/BotClientInteractionController.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc;
+using ShortP2P.MessengerServer.Contracts;
+
+namespace ShortP2P.MessengerServer.Api.Controllers;
+
+[ApiController]
+[Route($"{ApiRoutes.Prefix}/bot_server_interaction")]
+public class BotClientInteractionController
+{
+    
+}
---
+++ src/Server/ShortP2P.MessengerServer.Api/Controllers/BotDataFlowController.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using ShortP2P.MessengerServer.Contracts;
+
+namespace ShortP2P.MessengerServer.Api.Controllers;
+
+
+[ApiController]
+[Route($"{ApiRoutes.Prefix}/bot_data_flow")]
+public class BotDataFlowController
+{
+    
+}
